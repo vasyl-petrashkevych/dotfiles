cc18056df4e39bf85270a3f9f715543c9a68c930
Vasyl Petrashkevych <petrashkevych.v@gmail.com>
2025-03-05 14:45:41 +0200

    Update configuration

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -5,26 +5,31 @@ return {
 		{
 			"williamboman/mason-lspconfig.nvim",
 			event = "BufReadPre", -- Load before opening a file
-			opts = {
-				ensure_installed = {
-					"lua_ls",
-					"clangd",
-					"cmake",
-					"html",
-					"cssls",
-					"intelephense",
-					"stylelint_lsp",
-					"jsonls",
-					"graphql",
-					"typos_lsp",
-					"docker_compose_language_service",
-					"dockerls",
-					"bashls",
-					"emmet_ls",
-				},
-				automatic_installation = true,
-			},
 		},
 	},
+	config = function()
+		local mason = require("mason")
+		local mason_conf = require("mason-lspconfig")
+		mason.setup()
+		mason_conf.setup({
+			ensure_installed = {
+				"lua_ls",
+				"clangd",
+				"cmake",
+				"html",
+				"cssls",
+				"intelephense",
+				"stylelint_lsp",
+				"jsonls",
+				"graphql",
+				"typos_lsp",
+				"docker_compose_language_service",
+				"dockerls",
+				"bashls",
+				"emmet_ls",
+			},
+			automatic_installation = true,
+		})
+	end,
 	opts = {}, -- Mason will use default setup
 }
