e7d1cb0bced1af9c4817f467eae755db264697be
Vasyl Petrashkevych <petrashkevych.v@gmail.com>
2025-03-26 15:25:42 +0200

    Updates

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,20 @@
 return {
 	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"nvimtools/none-ls.nvim", -- Required for nvim-eslint
+		"MunifTanjim/eslint.nvim", -- nvim-eslint plugin
+	},
 	config = function()
-		local lspconfig = require("lspconfig")
+		local lsp_ok, lspconfig = pcall(require, "lspconfig")
+		if not lsp_ok then
+			return
+		end
+
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
 		local format_on_save = true -- Toggle this variable to enable/disable formatting on save
 
 		local function on_attach(client, bufnr)
@@ -17,13 +28,10 @@ return {
 			map("<leader>sh", vim.lsp.buf.signature_help, "Show signature help")
 			map("<leader>wa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
 			map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
-
 			map("<leader>wl", function()
 				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 			end, "List workspace folders")
-
 			map("<leader>D", vim.lsp.buf.type_definition, "Go to type definition")
-
 			map("<leader>ca", vim.lsp.buf.code_action, "Code action")
 			map("gr", vim.lsp.buf.references, "Show references")
 
@@ -32,7 +40,6 @@ return {
 					buffer = bufnr,
 					callback = vim.lsp.buf.document_highlight,
 				})
-
 				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 					buffer = bufnr,
 					callback = vim.lsp.buf.clear_references,
@@ -43,12 +50,24 @@ return {
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					buffer = bufnr,
 					callback = function()
-						vim.lsp.buf.format({ bufnr = bufnr })
+						vim.lsp.buf.format({ async = true })
 					end,
 				})
 			end
 		end
 
+		-- Load nvim-eslint if available
+		local eslint_ok, eslint = pcall(require, "eslint")
+		if eslint_ok then
+			eslint.setup({
+				bin = "eslint_d", -- Uses `eslint_d` for faster execution
+				code_actions = { enable = true },
+				diagnostics = { enable = true },
+				disable_if_no_config = true,
+			})
+		end
+
+		-- LSP Server Configurations
 		local servers = {
 			lua_ls = {
 				settings = {
@@ -60,50 +79,35 @@ return {
 			},
 			ts_ls = {
 				filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-				root_dir = require("lspconfig.util").root_pattern(
-					"package.json",
-					"tsconfig.json",
-					"jsconfig.json",
-					".git"
-				),
-				settings = {
-					completions = { completeFunctionCalls = true },
-				},
-			},
-			emmet_ls = {
-				filetypes = {
-					"html",
-					"css",
-					"scss",
-					"javascriptreact",
-					"typescriptreact",
-					"markdown",
-					"ejs",
-				},
-				init_options = {
-					html = {
-						options = {
-							["bem.enabled"] = true, -- Enable BEM support
-						},
-					},
-				},
-			},
-			cssls = {},
-			html = {
-				filetypes = { "html", "ejs" },
-				init_options = {
-					provideFormatter = true,
-				},
-			},
-			stylelint_lsp = {
-				filetypes = { "css", "scss", "less" },
-				settings = { stylelintplus = { autoFixOnSave = true, autoFixOnFormat = true } },
+				root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+				settings = { completions = { completeFunctionCalls = true } },
 			},
 			eslint = {
 				on_attach = function(client, bufnr)
 					on_attach(client, bufnr)
-					vim.api.nvim_create_autocmd("BufWritePre", { buffer = bufnr, command = "EslintFixAll" })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							if eslint_ok then
+								eslint.fix() -- Runs nvim-eslint fix on save
+							end
+							vim.lsp.buf.format({ async = true })
+						end,
+					})
 				end,
+			},
+			emmet_ls = {
+				filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact", "markdown", "ejs" },
+				init_options = { html = { options = { ["bem.enabled"] = true } } },
+			},
+			cssls = {},
+			html = {
+				filetypes = { "html", "ejs" },
+				init_options = { provideFormatter = true },
+			},
+			stylelint_lsp = {
+				filetypes = { "css", "scss", "less" },
+				settings = { stylelintplus = { autoFixOnSave = true, autoFixOnFormat = true } },
 			},
 			docker_compose_language_service = {},
 			dockerls = {},
@@ -115,12 +119,12 @@ return {
 					clangdFileStatus = true,
 				},
 				root_dir = function(fname)
-					return require("lspconfig.util").root_pattern(
+					return lspconfig.util.root_pattern(
 						"Makefile",
 						"configure.ac",
 						"compile_commands.json",
 						"CMakeLists.txt"
-					)(fname) or require("lspconfig.util").find_git_ancestor(fname)
+					)(fname) or lspconfig.util.find_git_ancestor(fname)
 				end,
 			},
 			cmake = {},
@@ -147,6 +151,18 @@ return {
 			},
 		}
 
+		-- Setup Mason for easy LSP installations
+		local mason_ok, mason = pcall(require, "mason")
+		local mason_lsp_ok, mason_lsp = pcall(require, "mason-lspconfig")
+
+		if mason_ok and mason_lsp_ok then
+			mason.setup()
+			mason_lsp.setup({
+				ensure_installed = vim.tbl_keys(servers), -- Auto-install LSPs
+			})
+		end
+
+		-- Initialize LSP servers
 		for server, config in pairs(servers) do
 			config.capabilities = capabilities
 			config.on_attach = config.on_attach or on_attach
