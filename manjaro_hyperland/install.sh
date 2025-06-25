df64205fa600f64707631d117893665a28736f75
Vasyl Petrashkevych <petrashkevych.v@gmail.com>
2025-06-25 22:58:10 +0300

    Add config for manjaro hyperland

---
+++ manjaro_hyperland/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "Manjaro After install part"
+# sudo pacman-mirrors --fasttrack
+sudo pacman -Syyu && sudo systemctl enable fstrim.timer && sudo systemctl start fstrim.timer  
+sudo pacman -S pavucontrol ufw gufw neovim kitty hyprland hyprpaper dunst pipewire waybar ranger xdg-desktop-portal-hyprland docker docker-compose flameshot stow bitwarden gtk-chtheme alacritty ttf-meslo-nerd php php-apache php-cgi php-fpm php-gd  php-embed php-intl php-redis php-snmp
+
+sudo pamac install lua-language-server fish lazygit docker docker-compose bat clang python-pip cmake-language-server
+
+sudo ufw enable && sudo systemctl enable ufw
+
+echo "Install NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+echo "alias v=\"nvim\"" >> .zshrc 
+
+echo "Reload .zshrc"
+source ~/.zshrc
+
+echo "Install nopde js"
+nvm install --lts
+
+echo "Dokcer post installation steps"
+sudo usermod -aG docker $USER 
+sudo systemctl enable docker.service 
+sudo systemctl enable containerd.service
+
+echo "Configure ssh keys"
+chmod 644 ~/.ssh/*
+chmod 600 ~/.ssh/work
+chmod 600 ~/.ssh/id_rsa
+
+echo "Create Wrok and Project folders"
+cd && mkdir Work && mkdir Projects
+
+echo "Clone dotfiles"
+git clone git@github.com:vasyl-petrashkevych/dotfiles.git
+
+npm install -g yarn emmet_ls vscode-langservers-extracted typescript typescript-language-server intelephense diagnostic-languageserver stylelint-lsp
+
+#composer global require php-stubs/wordpress-globals php-stubs/wordpress-stubs php-stubs/woocommerce-stubs php-stubs/acf-pro-stubs wpsyntex/polylang-stubs php-stubs/genesis-stubs php-stubs/wp-cli-stubs
