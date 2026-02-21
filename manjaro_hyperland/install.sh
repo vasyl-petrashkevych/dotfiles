d4fdcaea3cb91bda7acb19ec4a805a2ba7b7967d
Vasyl Petrashkevych <petrashkevych.v@gmial.com>
2026-02-22 01:37:32 +0200

    updates

--- manjaro_hyperland/install.sh
+++ manjaro_hyperland/install.sh
@@ -4,7 +4,7 @@ echo "Manjaro After install part"
 sudo pacman -Syyu && sudo systemctl enable fstrim.timer && sudo systemctl start fstrim.timer  
 sudo pacman -S pavucontrol ufw gufw neovim kitty hyprland hyprpaper dunst pipewire waybar ranger rofi xdg-desktop-portal-hyprland flameshot stow bitwarden alacritty ttf-meslo-nerd php php-apache php-cgi php-fpm php-gd  php-embed php-intl php-redis php-snmp blueman composer repgrep a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv opus wavpack x264 xvidcore
 
-sudo pamac install lua-language-server fish lazygit docker docker-compose docker-buildx bat clang python-pip valgrind polkit polkit-kde-agent polkit-qt5 lib32-polkit vls
+sudo pamac install lua-language-server fish lazygit docker docker-compose docker-buildx bat clang python-pip valgrind polkit polkit-kde-agent polkit-qt5 lib32-polkit vls luarocks tree-sitter lua fd polkit-gnome
 
 sudo ufw enable && sudo systemctl enable ufw
 
@@ -12,6 +12,7 @@ echo "Install NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 LINE='if [[ -f ~/.config/myzshrc.zsh ]]; then source ~/.config/myzshrc.zsh fi'
+
 # Check if it's already in .zshrc
 if ! grep -q 'source ~/.config/myzshrc.zsh' ~/.zshrc; then
   printf "\n# Load custom zsh config\n%s\n" "$LINE" >> ~/.zshrc
@@ -39,11 +40,8 @@ chmod 600 ~/.ssh/id_rsa
 echo "Create Wrok and Project folders"
 cd && mkdir Work && mkdir Projects
 
-echo "Clone dotfiles"
-git clone git@github.com:vasyl-petrashkevych/dotfiles.git
-
-npm install -g yarn vscode-langservers-extracted typescript typescript-language-server intelephense diagnostic-languageserver stylelint-lsp
+npm install -g eslint_d yarn vscode-langservers-extracted typescript typescript-language-server intelephense diagnostic-languageserver stylelint-lsp
 
 composer global require php-stubs/wordpress-globals php-stubs/wordpress-stubs php-stubs/woocommerce-stubs php-stubs/acf-pro-stubs wpsyntex/polylang-stubs php-stubs/genesis-stubs php-stubs/wp-cli-stubs
 
-cd ../ && stow manjaro_hyperland nvim alacritty backgrounds fonts idevimrc
+cd ../ && stow manjaro_hyperland nvim alacritty backgrounds fonts ideavimrc neovim
